f9055d0767e8c7f4b887001d9418a7b3a5729d0b
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2023-02-05 17:11:57 +0100

    Developed desktop and mobile

---
+++ main.js
@@ -0,0 +1,33 @@
+let ratingSelected = null;
+
+function ratingButtonHandler(btnIdentifier){
+    const btnPressed = document.getElementById(btnIdentifier);
+    
+    removeClassActive();
+    addClassActive(btnPressed);
+    setRating(btnPressed.innerHTML);
+}
+
+function removeClassActive(){
+    const activeRating = document.getElementsByClassName('btn-active');
+    if(activeRating.length > 0){
+        activeRating[0].classList.remove('btn-active');
+    }
+    
+}
+
+function addClassActive(element){
+    element.classList.add('btn-active');
+}
+
+function setRating(element){
+    ratingSelected = element;
+    console.log(ratingSelected);
+}
+
+function submitRating(){
+    document.getElementById('rating-state').style.display = 'none';
+    document.getElementById('thank-you-state').classList.remove('thank-you-state');
+    document.getElementById('thank-you-state').classList.add('thank-you-layout');
+    document.getElementById('ratingValue').innerHTML = ratingSelected;
+}
